6c979fa44972a1d48658bcc8cc925bf9d1abc87c
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-27 12:15:15 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+-- require_relative "./create"
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate",60,'M',"black","low",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",60,'M',"grey","high",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",60,'F',"white","low",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",60,'M',"black","low",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",60,'F',"black","high",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",60,'M',"brown","low",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",60,'F',"black","low",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,NULL,NULL,NULL,NULL,NULL);
